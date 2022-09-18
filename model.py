2bc70419745aac0c3668cb7a2f2689aafb034064
wubo2180 <15827403235@163.com>
2022-09-18 13:59:14 +0800

    add model

--- model.py
+++ model.py
@@ -18,4 +18,43 @@ class metaDynamicGCN(nn.Module):
         h = self.linear(h)
         return h
 
+class RecurrentGCN(nn.Module):
+    def __init__(self, args):
+        super(RecurrentGCN, self).__init__()
+        self.mode = False
+        self.RNNGCN = ''
+        if args.layer_mode == '0':
+            self.recurrent = GCNConv(args.input_dim, args.hidden_dim)
+        if args.layer_mode == '1':
+            self.recurrent = GConvGRU(args.input_dim, args.hidden_dim, 1)
+        elif args.layer_mode == '2':
+            self.recurrent = DCRNN(args.input_dim, args.hidden_dim, 1)
+        elif args.layer_mode == '3':
+            self.recurrent = GConvLSTM(args.input_dim, args.hidden_dim, 1)
+            self.RNNGCN = 'GConvLSTM'
+        elif args.layer_mode == '4':
+            self.recurrent = EvolveGCNH(args.num_nodes, args.input_dim)
+            self.linear_ = nn.Linear(args.input_dim,args.hidden_dim)
+            self.mode = True
+        elif args.layer_mode == '5':
+            self.recurrent = EvolveGCNO(args.input_dim)
+            self.linear_ = nn.Linear(args.input_dim,args.hidden_dim)
+            self.mode = True
+        elif args.layer_mode == '6':
+            self.recurrent = TGCN(args.input_dim, args.hidden_dim)
+        elif args.layer_mode == '7':
+            self.recurrent = A3TGCN(args.input_dim, args.hidden_dim,4)
+        elif args.layer_mode == '8':
+            self.recurrent = STConv(args.num_nodes,args.input_dim, args.hidden_dim,args.hidden_dim,1,1)
+        self.linear = nn.Linear(args.hidden_dim, 1)
 
+    def forward(self, x, edge_index, edge_weight):
+        if self.RNNGCN == 'GConvLSTM':
+            h, c = self.recurrent(x, edge_index, edge_weight)
+        else:
+            h = self.recurrent(x, edge_index, edge_weight)
+        if self.mode == True:
+            h = self.linear_(h)
+        h = F.relu(h)
+        h = self.linear(h)
+        return h
